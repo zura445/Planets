26986c95844502c6f950aab4bb91506b811f75d4
zura tetrashvili <zuratetra@gmail.com>
2024-08-05 17:30:58 +0400

    start header file and add router librari

---
+++ src/components/Header.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+function Header() {
+  return (
+    <header>
+      <nav>
+        <ul>
+          <li></li>
+        </ul>
+      </nav>
+    </header>
+  );
+}
+
+export default Header;
