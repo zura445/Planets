a19df2c33b70071ab2fea610b2a11ef0936bd0bc
zura tetrashvili <zuratetra@gmail.com>
2024-08-05 19:32:38 +0400

    add router structure

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,15 @@
 import Header from "./components/Header";
+import { Route, Routes } from "react-router";
+import Planet from "./components/Planet";
 
 function App() {
   return (
     <>
       <div className="">
         <Header />
+        <Routes>
+          <Route path="/:planet" element={<Planet />}></Route>
+        </Routes>
       </div>
     </>
   );
---
+++ src/components/Planet.tsx
@@ -0,0 +1,13 @@
+import data from "../data.json";
+import { useParams } from "react-router";
+
+function Planet() {
+  const params = useParams();
+  const planetName = params.planet;
+
+  const planet = data.find((planetObg) => planetObg.name === planetName);
+
+  return <div>{planet?.name}</div>;
+}
+
+export default Planet;
