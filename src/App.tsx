8f22f118be5b731debdb696e90e3d9542407f163
zura tetrashvili <zuratetra@gmail.com>
2024-08-05 19:09:51 +0400

    create header structure

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,11 @@
+import Header from "./components/Header";
+
 function App() {
   return (
     <>
-      <div className="">hgvjhbkjnkm</div>
+      <div className="">
+        <Header />
+      </div>
     </>
   );
 }
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,11 +1,18 @@
-import React from "react";
+import data from "../data.json";
+import { Link } from "react-router-dom";
 
 function Header() {
   return (
     <header>
       <nav>
         <ul>
-          <li></li>
+          {data.map((planet, index) => {
+            return (
+              <li key={index}>
+                <Link to={`/${planet.name}`}>{planet.name}</Link>
+              </li>
+            );
+          })}
         </ul>
       </nav>
     </header>
