7a55351f382e8dd97aaf5a7b53fdc4be26f9e9b6
zura tetrashvili <zuratetra@gmail.com>
2024-08-11 21:00:53 +0400

    finish burger menu

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -72,18 +72,32 @@ const Header: React.FC = () => {
         {isOpen && (
           <ul className="sm:hidden py-10">
             {data.map((planet, index) => (
-              <li key={index} className="px-8 py-2">
-                <Link
-                  className={`text-white hover:text-gray-300 ${
-                    location.pathname === `/${planet.name.toLowerCase()}`
-                      ? "font-bold"
-                      : ""
-                  }`}
-                  to={`/${planet.name.toLowerCase()}`}
-                  onClick={() => setIsOpen(false)}
-                >
-                  {planet.name}
-                </Link>
+              <li
+                key={index}
+                className="mx-8 py-2 flex justify-between items-center border-b border-gray-700 pb-3"
+              >
+                <div className="flex">
+                  <div
+                    className="w-5 h-5 rounded-full"
+                    style={{
+                      backgroundColor: planet?.style?.["background-color"],
+                    }}
+                  ></div>
+                  <Link
+                    className={`text-white hover:text-gray-300 ml-6 ${
+                      location.pathname === `/${planet.name.toLowerCase()}`
+                        ? "font-bold"
+                        : ""
+                    }`}
+                    to={`/${planet.name.toLowerCase()}`}
+                    onClick={() => setIsOpen(false)}
+                  >
+                    {planet.name}
+                  </Link>
+                </div>
+                <div className="mr-2">
+                  <img src="/images/arrow.png" alt="arrow" />
+                </div>
               </li>
             ))}
           </ul>
