49babdf65d4bb8294bdd8c3a6898e837b1af3739
zura tetrashvili <zuratetra@gmail.com>
2024-08-12 14:20:54 +0400

    finish mercury mobile version

--- src/components/Planet.tsx
+++ src/components/Planet.tsx
@@ -30,15 +30,39 @@ function Planet() {
           alt="planet image"
         />
       </div>
-      <div className="">
+      <div className="pb-12">
         <div className="text-center">
           <p className="text-white text-[40px]">{planet?.name}</p>
           <p className="text-xs text-white mt-4">{planet?.overview.content}</p>
         </div>
-        <div className="flex justify-center mt-8 text-white">
-          <p>Source</p>
-          <a href={planet?.overview.source}>Wikipedia</a>
-          <img src={planet?.overview.linkErrow} alt="" />
+        <div className="flex items-center justify-center mt-8 text-white">
+          <p>Source :</p>
+          <a href={planet?.overview.source} target="blank" className="ml-1">
+            Wikipedia
+          </a>
+          <img
+            src={planet?.overview.linkErrow}
+            className="w-3 h-3 ml-1"
+            alt="source link"
+          />
+        </div>
+        <div className="mt-6">
+          <div className="border px-6 py-3 flex justify-between mt-2">
+            <p>ROTATION TIME</p>
+            <p className="text-xl text-white">58.6 DAYS</p>
+          </div>
+          <div className="border px-6 py-3 flex justify-between mt-2">
+            <p>REVOLUTION TIME</p>
+            <p className="text-xl text-white">87.97 DAYS</p>
+          </div>
+          <div className="border px-6 py-3 flex justify-between mt-2">
+            <p>RADIUS</p>
+            <p className="text-xl text-white">2,439.7 KM</p>
+          </div>
+          <div className="border px-6 py-3 flex justify-between mt-2">
+            <p>AVERAGE TEMP.</p>
+            <p className="text-xl text-white">430°C</p>
+          </div>
         </div>
       </div>
     </div>
