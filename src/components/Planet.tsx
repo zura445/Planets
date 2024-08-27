e357261c0605d299846ea5adbf8318a671beed8e
zura tetrashvili <zuratetra@gmail.com>
2024-08-27 11:38:15 +0400

    finish markup

--- src/components/Planet.tsx
+++ src/components/Planet.tsx
@@ -94,91 +94,106 @@ function Planet() {
           SURFACE
         </p>
       </div>
-      <div className="px-8 block mt-[126px]">
-        <div className="m-auto flex justify-center items-center ">
-          <img
-            src={getImageSource()}
-            style={{
-              width: planet?.planetSize?.[deviceType]?.["width"],
-              height: planet?.planetSize?.[deviceType]?.["height"],
-            }}
-            alt="planet image"
-          />
-        </div>
-        <div className="pb-12 block md:flex">
-          <div className="md:mr-[69px]">
+      <div className="lg:px-[165px]">
+        <div className="px-8 block mt-[126px] lg:flex">
+          <div className="lg:w-[50%] w-full flex justify-center items-center ">
+            <img
+              src={getImageSource()}
+              style={{
+                width: planet?.planetSize?.[deviceType]?.["width"],
+                height: planet?.planetSize?.[deviceType]?.["height"],
+              }}
+              alt="planet image"
+            />
+          </div>
+          <div className="pb-12 block md:flex text-center md:text-left w-full lg:block lg:items-end lg:max-w-[350px]">
             <div className="">
-              <p className="text-white text-[40px]">{planet?.name}</p>
-              <p className="text-xs text-white mt-4">{getContent()}</p>
-            </div>
-            <div className="flex items-center justify-center mt-8 text-white">
-              <div className="flex items-center">
-                <p>Source :</p>
-                <a
-                  href={planet?.overview.source}
-                  target="blank"
-                  className="ml-1"
-                >
-                  Wikipedia
-                </a>
-                <img
-                  src={planet?.overview.linkErrow}
-                  className="w-3 h-3 ml-1"
-                  alt="source link"
-                />
+              <div className="">
+                <p className="text-white text-[40px]">{planet?.name}</p>
+                <p className="text-xs text-white mt-4">{getContent()}</p>
+              </div>
+              <div className="flex items-center mt-8 text-white md:hidden lg:flex">
+                <div className="flex items-center">
+                  <p>Source :</p>
+                  <a
+                    href={planet?.overview.source}
+                    target="blank"
+                    className="ml-1"
+                  >
+                    Wikipedia
+                  </a>
+                  <img
+                    src={planet?.overview.linkErrow}
+                    className="w-3 h-3 ml-1"
+                    alt="source link"
+                  />
+                </div>
               </div>
             </div>
+            <div className="hidden md:block md:ml-[69px] lg:ml-0 w-full justify-between text-xs mt-10">
+              <p
+                className={`py-5 cursor-pointer border text-white ${
+                  imageType === "overview"
+                    ? "border-[#D83A34] bg-[#D83A34]"
+                    : "text-gray-500"
+                }`}
+                onClick={() => setImageType("overview")}
+              >
+                <span className="px-7">01</span> OVERVIEW
+              </p>
+              <p
+                className={`py-5 cursor-pointer border text-white mt-4 ${
+                  imageType === "structure"
+                    ? "border-[#D83A34] bg-[#D83A34]"
+                    : "text-gray-500"
+                }`}
+                onClick={() => setImageType("structure")}
+              >
+                <span className="px-7">02</span> ITERNAL STRUCTURE
+              </p>
+              <p
+                className={`py-5 cursor-pointer border text-white mt-4 ${
+                  imageType === "surface"
+                    ? "border-[#D83A34] bg-[#D83A34]"
+                    : "text-gray-500"
+                }`}
+                onClick={() => setImageType("surface")}
+              >
+                <span className="px-7">03</span> SURFACE GEOLOGY
+              </p>
+            </div>
           </div>
-          <div className="hidden md:block w-full justify-between text-xs px-8 md:ml-10 lg:ml-20">
-            <p
-              className={`py-5 cursor-pointer border text-white ${
-                imageType === "overview"
-                  ? "border-[#D83A34] bg-[#D83A34]"
-                  : "text-gray-500"
-              }`}
-              onClick={() => setImageType("overview")}
-            >
-              <span className="px-7">01</span> OVERVIEW
-            </p>
-            <p
-              className={`py-5 cursor-pointer border text-white mt-4 ${
-                imageType === "structure"
-                  ? "border-[#D83A34] bg-[#D83A34]"
-                  : "text-gray-500"
-              }`}
-              onClick={() => setImageType("structure")}
-            >
-              <span className="px-7">02</span> ITERNAL STRUCTURE
-            </p>
-            <p
-              className={`py-5 cursor-pointer border text-white mt-4 ${
-                imageType === "surface"
-                  ? "border-[#D83A34] bg-[#D83A34]"
-                  : "text-gray-500"
-              }`}
-              onClick={() => setImageType("surface")}
-            >
-              <span className="px-7">03</span> SURFACE GEOLOGY
-            </p>
+        </div>
+        <div className="md:flex hidden items-center px-8 text-white lg:hidden">
+          <div className="flex items-center">
+            <p>Source :</p>
+            <a href={planet?.overview.source} target="blank" className="ml-1">
+              Wikipedia
+            </a>
+            <img
+              src={planet?.overview.linkErrow}
+              className="w-3 h-3 ml-1"
+              alt="source link"
+            />
           </div>
         </div>
-      </div>
-      <div className="mt-6 md:mt-0 w-full block md:flex md:justify-around">
-        <div className="border px-6 py-3 flex md:block justify-between mt-2 lg:w-[225px]">
-          <p>ROTATION TIME</p>
-          <p className="text-xl text-white md:mt-2">{planet?.rotation}</p>
-        </div>
-        <div className="border px-6 py-3 flex md:block justify-between mt-2 lg:w-[225px]">
-          <p>REVOLUTION TIME</p>
-          <p className="text-xl text-white md:mt-2">{planet?.revolution}</p>
-        </div>
-        <div className="border px-6 py-3 flex md:block justify-between mt-2 lg:w-[225px]">
-          <p>RADIUS</p>
-          <p className="text-xl text-white md:mt-2">{planet?.radius}</p>
-        </div>
-        <div className="border px-6 py-3 flex md:block justify-between mt-2 lg:w-[225px]">
-          <p>AVERAGE TEMP.</p>
-          <p className="text-xl text-white md:mt-2">{planet?.temperature}</p>
+        <div className="mt-7 w-full block md:flex md:justify-between px-8">
+          <div className="border px-6 py-3 flex md:block justify-between mt-2 lg:w-[225px]">
+            <p>ROTATION TIME</p>
+            <p className="text-xl text-white md:mt-2">{planet?.rotation}</p>
+          </div>
+          <div className="border px-6 py-3 flex md:block justify-between mt-2 lg:w-[225px]">
+            <p>REVOLUTION TIME</p>
+            <p className="text-xl text-white md:mt-2">{planet?.revolution}</p>
+          </div>
+          <div className="border px-6 py-3 flex md:block justify-between mt-2 lg:w-[225px]">
+            <p>RADIUS</p>
+            <p className="text-xl text-white md:mt-2">{planet?.radius}</p>
+          </div>
+          <div className="border px-6 py-3 flex md:block justify-between mt-2 lg:w-[225px]">
+            <p>AVERAGE TEMP.</p>
+            <p className="text-xl text-white md:mt-2">{planet?.temperature}</p>
+          </div>
         </div>
       </div>
     </>
